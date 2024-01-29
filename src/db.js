2b44b010f20a166cf340045172660f466fb832fe
Tair <tairjanametov@gmail.com>
2024-01-29 21:49:59 +0600

    first commit

---
+++ src/db.js
@@ -0,0 +1,7 @@
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import "firebase/auth";
+
+// Initialize Firebase
+export default firebase = initializeApp(firebaseConfig);
